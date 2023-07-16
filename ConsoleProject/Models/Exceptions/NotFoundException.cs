13f1dd0549cf1080e38ba5b353a56710b8f0cdd3
Miralam50 <miralamqs@code.edu.az>
2023-07-16 21:54:57 +0400

    Adding new changes

---
+++ ConsoleProject/Models/Exceptions/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleProject.Models.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+            
+        }
+
+    }
+}
